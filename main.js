ef38275d75d53afbc8cbe53f91f0de38a0d7c5ca
CoderJoe82 <coderjoe82@gmail.com>
2019-12-17 13:18:54 -0500

    Added button clicks to change turns from red to black and add game pieces

--- main.js
+++ main.js
@@ -1,13 +1,38 @@
 const gameHolder = document.querySelector(".game");
+let turn = "black";
 
-function createTowers(div) {
+function createTowers() {
     for (i = 0; i < 7; i++) {
         const gameTower = document.createElement("div");
         gameTower.classList.add("column");
-        div.appendChild(gameTower);
+        gameHolder.appendChild(gameTower);
     }
 }
+createTowers();
 
-window.onload = function() {
-    createTowers(gameHolder);
+const redBlackTurn = function(event) {
+    const gameColumns = event.currentTarget;
+    if (turn === "red") {
+        if (gameColumns.childElementCount < 6) {
+            const gamePiece = document.createElement("div");
+            this.appendChild(gamePiece);
+        } else {
+            return;
+        }
+        turn = "black";
+    } else if (turn === "black") {
+        if (gameColumns.childElementCount < 6) {
+            const gamePiece = document.createElement("div");
+            this.appendChild(gamePiece);
+        } else {
+            return;
+        }
+        turn = "red";
+    }
+    console.log(turn);
+}
+
+const gameClicker = document.querySelectorAll(".column");
+for (i = 0; i < gameClicker.length; i++) {
+    gameClicker[i].addEventListener("click", redBlackTurn);
 }
