d51d44658e739ae33dc630e73aa81b2799c7c606
CoderJoe82 <coderjoe82@gmail.com>
2019-12-17 17:19:22 -0500

    Added boxes to put game pieces in on anywhere column is clicked

--- main.js
+++ main.js
@@ -2,34 +2,111 @@ const gameHolder = document.querySelector(".game");
 let turn = "black";
 
 function createTowers() {
-    for (i = 0; i < 7; i++) {
+    for (j = 0; j < 7; j++) {
         const gameTower = document.createElement("div");
         gameTower.classList.add("column");
         gameHolder.appendChild(gameTower);
+        for (i = 0; i < 6; i++) {
+            const cellCreator = document.createElement("div");
+            cellCreator.classList.add("cells");
+            gameTower.appendChild(cellCreator);
+        }
     }
 }
 createTowers();
 
 const redBlackTurn = function(event) {
-    const gameColumns = event.currentTarget;
     if (turn === "red") {
-        if (gameColumns.childElementCount < 6) {
-            const gamePiece = document.createElement("div");
-            this.appendChild(gamePiece);
-        } else {
+        if (event.currentTarget.lastElementChild.childElementCount === 0) {
+            const redPieceCreator = document.createElement("div");
+            redPieceCreator.classList.add("redpiece");
+            event.currentTarget.lastElementChild.appendChild(redPieceCreator);
+            turn = "black";
+            console.log(turn);
+            return;
+        } else if (event.currentTarget.lastElementChild.previousElementSibling.childElementCount === 0) {
+            const redPieceCreator = document.createElement("div");
+            redPieceCreator.classList.add("redpiece");
+            event.currentTarget.lastElementChild.previousElementSibling.appendChild(redPieceCreator);
+            turn = "black";
+            console.log(turn);
+            return;
+        } else if (event.currentTarget.lastElementChild.previousElementSibling.previousElementSibling.childElementCount === 0) {
+            const redPieceCreator = document.createElement("div");
+            redPieceCreator.classList.add("redpiece");
+            event.currentTarget.lastElementChild.previousElementSibling.previousElementSibling.appendChild(redPieceCreator);
+            turn = "black";
+            console.log(turn);
+            return;
+        } else if (event.currentTarget.lastElementChild.previousElementSibling.previousElementSibling.previousElementSibling.childElementCount === 0) {
+            const redPieceCreator = document.createElement("div");
+            redPieceCreator.classList.add("redpiece");
+            event.currentTarget.lastElementChild.previousElementSibling.previousElementSibling.previousElementSibling.appendChild(redPieceCreator);
+            turn = "black";
+            console.log(turn);
+            return;
+        } else if (event.currentTarget.lastElementChild.previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling.childElementCount === 0) {
+            const redPieceCreator = document.createElement("div");
+            redPieceCreator.classList.add("redpiece");
+            event.currentTarget.lastElementChild.previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling.appendChild(redPieceCreator);
+            turn = "black";
+            console.log(turn);
+            return;
+        } else if (event.currentTarget.lastElementChild.previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling.childElementCount === 0) {
+            const redPieceCreator = document.createElement("div");
+            redPieceCreator.classList.add("redpiece");
+            event.currentTarget.lastElementChild.previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling.appendChild(redPieceCreator);
+            turn = "black";
+            console.log(turn);
             return;
         }
-        turn = "black";
     } else if (turn === "black") {
-        if (gameColumns.childElementCount < 6) {
-            const gamePiece = document.createElement("div");
-            this.appendChild(gamePiece);
-        } else {
+        console.log(event.currentTarget.lastElementChild);
+        console.log(event.currentTarget.lastElementChild.lastElementChild);
+        if (event.currentTarget.lastElementChild.childElementCount === 0) {
+            const blackPieceCreator = document.createElement("div");
+            blackPieceCreator.classList.add("blackpiece");
+            event.currentTarget.lastElementChild.appendChild(blackPieceCreator);
+            turn = "red";
+            console.log(turn);
+            return;
+        } else if (event.currentTarget.lastElementChild.previousElementSibling.childElementCount === 0) {
+            const blackPieceCreator = document.createElement("div");
+            blackPieceCreator.classList.add("blackpiece");
+            event.currentTarget.lastElementChild.previousElementSibling.appendChild(blackPieceCreator);
+            turn = "red";
+            console.log(turn);
+            return;
+        } else if (event.currentTarget.lastElementChild.previousElementSibling.previousElementSibling.childElementCount === 0) {
+            const blackPieceCreator = document.createElement("div");
+            blackPieceCreator.classList.add("blackpiece");
+            event.currentTarget.lastElementChild.previousElementSibling.previousElementSibling.appendChild(blackPieceCreator);
+            turn = "red";
+            console.log(turn);
+            return;
+        } else if (event.currentTarget.lastElementChild.previousElementSibling.previousElementSibling.previousElementSibling.childElementCount === 0) {
+            const blackPieceCreator = document.createElement("div");
+            blackPieceCreator.classList.add("blackpiece");
+            event.currentTarget.lastElementChild.previousElementSibling.previousElementSibling.previousElementSibling.appendChild(blackPieceCreator);
+            turn = "red";
+            console.log(turn);
+            return;
+        } else if (event.currentTarget.lastElementChild.previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling.childElementCount === 0) {
+            const blackPieceCreator = document.createElement("div");
+            blackPieceCreator.classList.add("blackpiece");
+            event.currentTarget.lastElementChild.previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling.appendChild(blackPieceCreator);
+            turn = "red";
+            console.log(turn);
+            return;
+        } else if (event.currentTarget.lastElementChild.previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling.childElementCount === 0) {
+            const blackPieceCreator = document.createElement("div");
+            blackPieceCreator.classList.add("blackpiece");
+            event.currentTarget.lastElementChild.previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling.appendChild(blackPieceCreator);
+            turn = "red";
+            console.log(turn);
             return;
         }
-        turn = "red";
     }
-    console.log(turn);
 }
 
 const gameClicker = document.querySelectorAll(".column");
