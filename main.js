fd40ed331883f4e1d9b9d6b94e91fdc8c45d0838
CoderJoe82 <coderjoe82@gmail.com>
2019-12-17 12:28:41 -0500

    Adding board creator

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
-function createTowers() {
+const gameHolder = document.querySelector(".game");
+
+function createTowers(div) {
     for (i = 0; i < 7; i++) {
         const gameTower = document.createElement("div");
         gameTower.classList.add("column");
-        const gameHolder = document.querySelector(".game");
-        gameHolder.appendChild(gameTower);
+        div.appendChild(gameTower);
     }
 }
 
 window.onload = function() {
-    createTowers();
+    createTowers(gameHolder);
 }
